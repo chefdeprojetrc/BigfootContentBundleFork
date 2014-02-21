63a2d6e33aa4bf8b2fc757a7f1e3d46e111ec564
Guillaume Manen <g.manen@c2is.fr>
2014-02-21 12:07:38 +0100

    Menu access restricted to admin

--- Listener/MenuListener.php
+++ Listener/MenuListener.php
@@ -7,12 +7,26 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Doctrine\ORM\EntityManager;
 
 use Bigfoot\Bundle\CoreBundle\Event\MenuEvent;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Menu Listener
  */
 class MenuListener implements EventSubscriberInterface
 {
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContextInterface
+     */
+    private $security;
+
+    /**
+     * @param SecurityContextInterface $security
+     */
+    public function __construct(SecurityContextInterface $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * Get subscribed events
      *
@@ -33,51 +47,53 @@ class MenuListener implements EventSubscriberInterface
         $menu        = $event->getSubject();
         $contentMenu = $menu->getChild('content');
 
-        $contentMenu->addChild(
-            'template',
-            array(
-                'label'  => 'Template',
-                'route'  => 'admin_contentbundle_template',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_contentbundle_template_new',
-                        'admin_contentbundle_template_edit'
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            $contentMenu->addChild(
+                'template',
+                array(
+                    'label'  => 'Template',
+                    'route'  => 'admin_contentbundle_template',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_contentbundle_template_new',
+                            'admin_contentbundle_template_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon'  => 'list-alt',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon'  => 'list-alt',
                 )
-            )
-        );
+            );
 
-        $contentMenu->addChild(
-            'page',
-            array(
-                'label'  => 'Page',
-                'route'  => 'admin_page',
-                'extras' => array(
-                    'routes' => array(
-                        'admin_page_new',
-                        'admin_page_edit'
+            $contentMenu->addChild(
+                'page',
+                array(
+                    'label'  => 'Page',
+                    'route'  => 'admin_page',
+                    'extras' => array(
+                        'routes' => array(
+                            'admin_page_new',
+                            'admin_page_edit'
+                        )
+                    ),
+                    'linkAttributes' => array(
+                        'icon'  => 'list-alt',
                     )
-                ),
-                'linkAttributes' => array(
-                    'icon'  => 'list-alt',
                 )
-            )
-        );
+            );
 
-        $structureMenu = $menu->getChild('structure');
+            $structureMenu = $menu->getChild('structure');
 
-        $structureMenu->addChild(
-            'widget',
-            array(
-                'label'          => 'Widget',
-                'route'          => 'admin_dashboard',
-                'linkAttributes' => array(
-                    'icon'  => 'list-alt',
+            $structureMenu->addChild(
+                'widget',
+                array(
+                    'label'          => 'Widget',
+                    'route'          => 'admin_dashboard',
+                    'linkAttributes' => array(
+                        'icon'  => 'list-alt',
+                    )
                 )
-            )
-        );
+            );
+        }
     }
 }
